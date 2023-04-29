4e4138507b6566a789bf08cd464c21ee0b0c4213
alvaromoralesmatrix <alvaro.morales.matrix@gmail.com>
2023-04-28 21:58:29 -0500

    fix:mensaje con mucho cariño

--- src/application/controllers/helloController.ts
+++ src/application/controllers/helloController.ts
@@ -1,27 +1,22 @@
 import { responseSuccess, responseFail } from '../helpers/responses';
 
-const data = 
-    {
-        data: [
-            {
-                name: 'Alvaro Anderson',
-                lastName: 'Morales caballero',
-                work: 'developer',
-                company: 'NTTDATA',
-                description: 'data for test'
-            }
-        ],
-        message: "Data Enviada con exito"
+const data = {
+    version: '1.0',
+    response: {
+      outputSpeech: {
+        type: 'PlainText',
+        text: 'hola elizabeth, alvaro te ama mucho y dice que res una hermosura de mujer'
+      },
+      shouldEndSession: true
     }
-;
+  };
 export const getHelloWord = async(event) =>{
     let response = null;
     try {
         response = responseSuccess(data,200);
     } catch (error) {
         response = responseFail({ 
-            data:[],
-            message: "Error inesperado"
+            data:[]
         },400);
     }
     return response;
