1fff51332e3e44e99ea8427a776a1b57e669adf1
alvaromoralesmatrix <alvaro.morales.matrix@gmail.com>
2023-04-28 22:39:19 -0500

    fix:incorporando mensaje expresivo

--- src/infraestructure/handlers/hello-handler.ts
+++ src/infraestructure/handlers/hello-handler.ts
@@ -1,7 +1,30 @@
-import  {HttpResponse} from '../helpers/response';
-import { getHelloWord as getHelloController} from '@application/controllers/helloController';
+import {
+  ErrorHandler,
+  HandlerInput,
+  RequestHandler,
+  SkillBuilders,
+} from 'ask-sdk-core';
+import {
+  Response,
+  SessionEndedRequest,
+} from 'ask-sdk-model';
 
- export const getHello = async (event) => {
-  const { data } = await getHelloController(event);
-  return HttpResponse._200(data);
+export const LaunchRequestHandler : RequestHandler = {
+  canHandle(handlerInput : HandlerInput) : boolean {
+    const request = handlerInput.requestEnvelope.request;
+    return request.type === 'LaunchRequest';        
+  },
+  handle(handlerInput : HandlerInput) : Response {
+    const speechText = 'hola elizabeth, alvaro te ama mucho y dice que res una hermosura de mujer';
+
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .reprompt(speechText)
+      .withSimpleCard('hola elizabeth, alvaro te ama mucho y dice que res una presiosura de mujer!', speechText)
+      .getResponse();
+  },
 };
+
+
+
+
